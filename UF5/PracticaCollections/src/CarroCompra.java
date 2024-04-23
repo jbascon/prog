4a478587f15e03c5eaf8cdada2c93fbf0e0270a3
jbascon <j.bascon@sapalomera.cat>
2024-04-23 17:03:02 +0200

    He creat la classe del carro de la compra amb el seu constructor i les funcions per agregar producte i calcular el preu total de tot el carro.

---
+++ UF5/PracticaCollections/src/CarroCompra.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class CarroCompra {
+    private List<Producte> productes;
+    private Set<Alimentacio> aliments;
+    private Set<Textil> textils;
+    private Set<Electronica> electronics;
+
+    public CarroCompra(List<Producte> productes, Set<Alimentacio> aliments, Set<Textil> textils, Set<Electronica> electronics) {
+        this.productes = new LinkedList<>();
+        this.aliments = new HashSet<>();
+        this.textils = new HashSet<>();
+        this.electronics = new HashSet<>();
+
+
+    }
+
+    public void agregarProducte(Producte producte) {
+        productes.add(producte);
+        if (producte instanceof Alimentacio) {
+            aliments.add((Alimentacio) producte);
+        } else if (producte instanceof Textil) {
+            textils.add((Textil) producte);
+        } else if (producte instanceof Electronica) {
+            electronics.add((Electronica) producte);
+        }
+    }
+
+    public double calcularPrecioTotal() {
+        return productes.stream().mapToDouble(Producte::getPreu).sum();
+    }
+}
--- UF5/PracticaCollections/src/Textil.java
+++ UF5/PracticaCollections/src/Textil.java
@@ -1,4 +1,4 @@
-public class Textil extends Producte{
+public class Textil extends Producte implements Comparable<Textil>{
     private String composicioTextil;
 
     public Textil(double preu, String nom, int codiBarres, String composicioTextil) {
@@ -9,8 +9,12 @@ public class Textil extends Producte{
     public String getComposicioTextil() {
         return composicioTextil;
     }
-
     public void setComposicioTextil(String composicioTextil) {
         this.composicioTextil = composicioTextil;
+    }
+
+    @Override
+    public int compareTo(Textil altreTextil) {
+        return this.composicioTextil.compareTo(altreTextil.getComposicioTextil());
     }
 }
